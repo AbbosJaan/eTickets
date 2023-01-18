c2002bd99de11992ca5c937b7aa18f229d96f7a8
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-01-18 12:08:14 +0500

    added OrdersController

---
+++ eTickets/Controllers/OrdersController.cs
@@ -0,0 +1,29 @@
+﻿using eTickets.Data.Cart;
+using eTickets.Data.Services;
+using eTickets.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IMoviesSerivce _moviesSerivce;
+        private readonly ShoppingCart _shoppingCart;
+        public OrdersController(IMoviesSerivce moviesSerivce, ShoppingCart shoppingCart)
+        {
+            _moviesSerivce = moviesSerivce;
+            _shoppingCart = shoppingCart;
+        }
+        public IActionResult Index()   
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCardItems = items;
+            var response = new ShoppingCartVM()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(response);
+        }
+    }
+}
