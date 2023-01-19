50238532153110cb127fd01d918f50faea64fb5e
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-01-19 14:32:46 +0500

    register view added

---
+++ eTickets/Data/ViewModels/RegisterVM.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Data.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required(ErrorMessage = "Full name is required")]
+        [Display(Name = "Full name")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Email address is required")]
+        [Display(Name ="Email address")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]   
+        public string Password { get; set; }
+
+
+        [Required(ErrorMessage = "Confirm password is requiered")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password do not match")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
