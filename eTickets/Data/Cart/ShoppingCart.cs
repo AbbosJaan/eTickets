250c269265e91ca89214e794032843a2d486a4fa
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-01-19 20:39:40 +0500

    order controller index page bug fixed

--- eTickets/Data/Cart/ShoppingCart.cs
+++ eTickets/Data/Cart/ShoppingCart.cs
@@ -68,6 +68,7 @@ namespace eTickets.Data.Cart
             .Where(n => n.ShoppingCartId == ShoppingCartId)
             .Select(n => n.Movie.Price * n.Amount).Sum();
 
+
         public async Task ClearShoppingCartAsync()
         {
             var items = await _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).ToListAsync();
